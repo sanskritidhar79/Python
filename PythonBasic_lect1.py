db869149fdf5eab09b4d0347b45760b9c66ac0cf
sanskritidhar79 <43353430+sanskritidhar79@users.noreply.github.com>
2025-04-15 07:46:29 +0530

    Add files via upload
    Python Basics Lecture 1 - Airthematic , logical and type conversion

---
+++ PythonBasic_lect1.py
@@ -0,0 +1,88 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red15\green112\blue1;\red245\green245\blue245;\red0\green0\blue0;
+\red19\green85\blue52;\red101\green76\blue29;\red157\green0\blue210;\red0\green0\blue255;\red144\green1\blue18;
+\red31\green99\blue128;}
+{\*\expandedcolortbl;;\cssrgb\c0\c50196\c0;\cssrgb\c96863\c96863\c96863;\cssrgb\c0\c0\c0;
+\cssrgb\c6667\c40000\c26667;\cssrgb\c47451\c36863\c14902;\cssrgb\c68627\c0\c85882;\cssrgb\c0\c0\c100000;\cssrgb\c63922\c8235\c8235;
+\cssrgb\c14510\c46275\c57647;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 # Airthematic operation\cf0 \cb1 \strokec4 \
+\cf2 \cb3 \strokec2 #Addition\cf0 \cb1 \strokec4 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3 a= \cf5 \strokec5 1\cf0 \cb1 \strokec4 \
+\cb3 b=\cf5 \strokec5 5\cf0 \cb1 \strokec4 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf6 \cb3 \strokec6 print\cf0 \strokec4 (a+b)\cb1 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3 a *=\cf5 \strokec5 3\cf0 \strokec4   \cf2 \strokec2 #multiplies a with itselef\cf0 \cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf6 \cb3 \strokec6 print\cf0 \strokec4 (a)\cb1 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3 d = b<a \cf2 \strokec2 #comaprsion opeartor \cf0 \cb1 \strokec4 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf6 \cb3 \strokec6 print\cf0 \strokec4 (d) \cf2 \strokec2 # returns true or false\cf0 \cb1 \strokec4 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf2 \cb3 \strokec2 #Logical \cf0 \cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf7 \cb3 \strokec7 if\cf0 \strokec4  a>\cf5 \strokec5 8\cf0 \strokec4  \cf8 \strokec8 and\cf0 \strokec4  b>\cf5 \strokec5 2\cf0 \strokec4 :\cb1 \
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3   \cf6 \strokec6 print\cf0 \strokec4 (\cf9 \strokec9 "I am greate"\cf0 \strokec4 )\cb1 \
+\pard\pardeftab720\partightenfactor0
+\cf7 \cb3 \strokec7 else\cf0 \strokec4 :\cb1 \
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3   \cf6 \strokec6 print\cf0 \strokec4 (\cf9 \strokec9 "I am more greate"\cf0 \strokec4 )\cb1 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf7 \cb3 \strokec7 if\cf0 \strokec4  a>\cf5 \strokec5 8\cf0 \strokec4  \cf8 \strokec8 or\cf0 \strokec4  b>\cf5 \strokec5 2\cf0 \strokec4 :\cb1 \
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3   \cf6 \strokec6 print\cf0 \strokec4 (\cf9 \strokec9 "I am greate"\cf0 \strokec4 )\cb1 \
+\
+\
+\pard\pardeftab720\partightenfactor0
+\cf2 \cb3 \strokec2 ##### Type Casting #####\cf0 \cb1 \strokec4 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3 x1= \cf9 \strokec9 "hello"\cf0 \cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf6 \cb3 \strokec6 print\cf0 \strokec4 (\cf10 \strokec10 type\cf0 \strokec4 (x1))\cb1 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3 x2 = \cf5 \strokec5 32.22\cf0 \cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf6 \cb3 \strokec6 print\cf0 \strokec4 (\cf10 \strokec10 type\cf0 \strokec4 (x2))\cb1 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3 x3 = \cf9 \strokec9 "32.22"\cf0 \cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf6 \cb3 \strokec6 print\cf0 \strokec4 (\cf10 \strokec10 type\cf0 \strokec4 (x3))\cb1 \
+\
+\
+\pard\pardeftab720\partightenfactor0
+\cf2 \cb3 \strokec2 # from one data type to another\cf0 \cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3 x4 = \cf10 \strokec10 float\cf0 \strokec4 ((x3))\cb1 \
+\pard\pardeftab720\partightenfactor0
+\cf6 \cb3 \strokec6 print\cf0 \strokec4 (\cf10 \strokec10 type\cf0 \strokec4 (x4))\cb1 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3 x5 = \cf8 \strokec8 True\cf0 \cb1 \strokec4 \
+\cb3 x6 = \cf8 \strokec8 not\cf0 \strokec4  x5\cb1 \
+\pard\pardeftab720\partightenfactor0
+\cf6 \cb3 \strokec6 print\cf0 \strokec4 (x6)\cb1 \
+\cf6 \cb3 \strokec6 print\cf0 \strokec4 (\cf10 \strokec10 type\cf0 \strokec4 (x6))\cb1 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf2 \cb3 \strokec2 # Datatypes in python - int , float , string , boolean , complex(using iota)\cf0 \cb1 \strokec4 \
+\
+}
